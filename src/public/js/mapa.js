db0f50f9d03efafa8ad79dc04db74d7196b8453b
AusiasBM <ausiasbm@gmail.com>
2021-11-22 23:38:55 +0100

    landing page parte de mapa casi terminada

---
+++ src/public/js/mapa.js
@@ -0,0 +1,33 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 38.9965055, lng: -0.1674364},
+        zoom: 15,
+        mapTypeId: 'hybrid', // Tipo de mapa que se va ha dibujar (mapa con imagenes de satelite, carreteras y calles
+        styles: [ // personalizar estilos del mapa
+            {
+                featureType: 'poi', // Ocultar puntos de interes: negocios,etc.
+                stylers: [{visibility: 'off'}]
+            },
+            {
+                featureType: 'transit', // Ocultar estaciones de tren, paradas de autobus, etc.
+                stylers: [{visibility: 'off'}]
+            }
+        ],
+        mapTypeControl: false, // Ocultar control de tipo de mapa
+        streetViewControl: false, // ocultar botón que permite pasar a stricview
+        rotateControl: false // ocultar
+  });
+
+
+  map.setTilt(0); // Forzar que siempre esté en la vista desde arriba.
+
+  infoWindow = new google.maps.InfoWindow();
+
+
+  var marker = new google.maps.Marker({
+        position: {lat: 38.9965055, lng: -0.1654364},
+        map: map,
+        title: 'UPV Gandia'
+    });
+}

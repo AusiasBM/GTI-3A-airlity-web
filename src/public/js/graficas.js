3c31b76a1e95f39dfea25cdc4f3b09b8c753d10c
AusiasBM <ausiasbm@gmail.com>
2021-11-24 12:58:47 +0100

    practicamente maquetada

--- src/public/js/graficas.js
+++ src/public/js/graficas.js
@@ -1,7 +1,4 @@
-var densityCanvas = document.getElementById("densityChart");
-
-Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = 18;
+var densityCanvas = document.getElementById("chartIca");
 
 var densityData = {
   label: 'Density of Planets (kg/m3)',
@@ -9,6 +6,26 @@ var densityData = {
 };
 
 var barChart = new Chart(densityCanvas, {
+  type: 'bar',
+  data: {
+    labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+    datasets: [densityData]
+  }
+});
+
+var densityCanvas1 = document.getElementById("chartIca1");
+
+var barChart = new Chart(densityCanvas1, {
+  type: 'bar',
+  data: {
+    labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+    datasets: [densityData]
+  }
+});
+
+var densityCanvas2 = document.getElementById("chartIca2");
+
+var barChart = new Chart(densityCanvas2, {
   type: 'bar',
   data: {
     labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],

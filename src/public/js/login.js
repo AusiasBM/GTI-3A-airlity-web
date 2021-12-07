7f2c6ac9916dea07755e7046c0df1701ca324bee
AusiasBM <ausiasbm@gmail.com>
2021-12-07 13:05:29 +0100

    token recibido

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -9,19 +9,18 @@ function login(evento){
         mode: "cors",
         headers: {"correo": document.getElementById('label_correo').value, "contrasenya": document.getElementById('label_contrasenya').value},
     }).then(function (resp){
-        console.log(resp.json())
         
         if(resp.status == 200){
             //location.href = './app.html';
+            return resp.json()
         }else{
             document.getElementById('textoErrorLogin').innerHTML = `
                 <h6>Algo no está bién</h6>
             `
         }
-        return resp.json()
-    }).then(
-        //(res) => console.log(res)
-    ).catch(
+    }).then( function(res){
+        console.log(res.data.token)
+    }).catch(
         (err) => console.log(err)
     );
 }

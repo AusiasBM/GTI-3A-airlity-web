50ec8bcf6c21a9ae796e4f30508c728efc9f0198
AusiasBM <ausiasbm@gmail.com>
2021-12-17 00:39:35 +0100

    filtro por contaminate hecho

--- src/public/js/logicaFake.js
+++ src/public/js/logicaFake.js
@@ -18,6 +18,9 @@ function obtenerUltimasMediciones(){
     // Hacemos la petición get
     fetch(IP_PUERTO + '/ultimasMediciones/20', {
         method : 'get', 
+        headers: {
+            'Authorization': sessionStorage.getItem('token')
+        }
     })
     // Esperamos la respuesta
     .then(function (respuesta) {
@@ -128,6 +131,9 @@ function informeSensores(){
     // Hacemos la petición get
     fetch(IP_PUERTO + '/informeSensores?ciudad=Gandia&tipoMedicion=NO2', {
         method : 'get', 
+        headers: {
+            'Authorization': sessionStorage.getItem('token')
+        }
     })
     // Esperamos la respuesta
     .then(function (respuesta) {
@@ -172,8 +178,6 @@ function informeSensores(){
                                 media Global = ${element.mediaGlobal}
                                 <br>
                                 media Sensor = ${element.mediaSensor}
-                                <br>
-                                sensor = ${element.sensor}
                                 <br>
                                 valores Irregulares No Validos = ${element.valoresIrregularesNoValidos}
                                 <br>
@@ -259,3 +263,111 @@ function sensoresInactivos(){
     })
 
 }
+
+
+function datosGraficaUsuario(){
+
+
+    // Hacemos la petición get
+    fetch(IP_PUERTO + '/datosGraficaUsuario?fechaIni=1637020800&fechaFin=1637193599', {
+        method : 'get', 
+        headers: {
+            'Authorization': sessionStorage.getItem('token')
+        }
+    })
+    // Esperamos la respuesta
+    .then(function (respuesta) {
+        if(respuesta.status != 200){
+            document.getElementById('listaInactivos').innerHTML = `
+                    <h6>Algo no ha salido bien</h6>`
+        }
+        return respuesta.json();
+    })
+    // Trabajamos con la respuesta para mostrarla.
+    .then((res) => {
+        console.log(res); // Depuración
+        
+        // listaRespJson = res;
+        // num = 0;
+
+        // // Esto lo hacemos para cuando apretemos el botón compruebe si ya hay mediciones mostrandose.
+        // if(!peticionGet) // En caso de que hayan peticiones mostrandose, las limpia
+        //     document.getElementById('listaInactivos').innerHTML = ``;
+        // else // En caso de que no, pone que apartidir de ahora si.
+        //     peticionGet = true;
+        
+
+        // // Añadimos las mediciones al html
+        // listaRespJson.forEach(element => {
+
+        //     var d = new Date(element.fechaRegistro);
+
+        //     document.getElementById('listaInactivos').innerHTML += `
+
+        //         <div class="card border-primary mb-3 mw-100">
+        //             <div class="card-header">Medición ${num}</div>
+        //             <div class="card-body">
+        //                 <h6 class="card-title">correo Usuario: ${element.correoUsuario}</h6>
+        //                 <p> 
+        //                     fecha Registro = ${d.toLocaleDateString()}
+        //                     <br>
+        //                     macSensor = ${element.macSensor}
+        //                     <br>
+        //                     tipo Medicion = ${element.tipoMedicion}
+                            
+        //                 </p>
+        //             </div>
+        //         </div>
+            
+        //     `
+        //     num++;
+            
+        // });
+
+        // if(num == 0){
+        //     document.getElementById('listaInactivos').innerHTML += `<h3>No hay</h3>`
+        // }
+    })
+
+}
+
+
+
+// <script>
+// const ctx = document.getElementById('myChart').getContext('2d');
+// const myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
+
+// </script>
